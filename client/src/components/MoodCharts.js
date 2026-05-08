e32241d41c2999d55a0b7169bad26f749057e03e
Ray-Xavier-2021 <horatioxhanley@gmail.com>
2026-05-08 18:33:39 -0400

    Add Mood Chart file to Components dir and installed needed packages

---
+++ client/src/components/MoodCharts.js
@@ -0,0 +1,42 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+// Mood Chart function generates chart based on mood data
+function MoodChart({ entries }) {
+  const moodCounts = Object.entries(
+    entries.reduce((count, entry) => {
+      count[entry.mood] = (count[entry.mood] || 0) + 1;
+      return count;
+    }, {})
+  ).map(([mood, count]) => ({
+    mood,
+    count,
+  }));
+
+  if (entries.length === 0) {
+    return null;
+  }
+
+  return (
+    <section className="chart-section">
+      <h2>Mood Frequency</h2>
+
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={moodCounts}>
+          <XAxis dataKey="mood" />
+          <YAxis allowDecimals={false} />
+          <Tooltip />
+          <Bar dataKey="count" />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
+  );
+}
+
+export default MoodChart;
