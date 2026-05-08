bbde8cfaa21aa309690801c49ad19e0645b21d3f
Ray-Xavier-2021 <horatioxhanley@gmail.com>
2026-05-08 17:23:19 -0400

    Updated API Logic

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,22 @@
 import axios from "axios";
 
-// Backend URL
+// Backend API URL
+const API_URL = "http://localhost:5000/api/entries";
+
+// Get Entries
+export const getEntries = () => axios.get(API_URL)
+
+// Create Entry
+export const createEntry = (entryData) => axios.post(API_URL, entryData)
+
+// Update Entry
+export const updateEntry = (id, entryData) => axios.put(`${API_URL}/${id}`, entryData)
+
+// Delete Entry
+export const deleteEntry = (id) => axios.delete(`${API_URL}/${id}`)
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: ,
 });
 
 export default API;
