c977dd72802532aac2d7acafbc6c644c96845f94
Ray-Xavier-2021 <horatioxhanley@gmail.com>
2026-05-09 15:10:23 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import "./App.css"
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import "./App.css";
 
 function App() {
-
   return (
-
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={< Dashboard/>} />
-        <Route path="/login" element={< Login/>} />
-        <Route path="/register" element={< Register/>} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
-
   );
-
 }
 
 export default App;
