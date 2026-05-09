9d852522d2e8ee0e0783a689c2f3daaec136c313
Ray-Xavier-2021 <horatioxhanley@gmail.com>
2026-05-09 13:19:48 -0400

    Update Login file

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,23 @@ import { useState } from "react";
 import { loginUser } from "../services/authServices";
 
 function Login() {
+  // Form/User Data State
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  // Error State
+  const [error, setError] = useState("");
+
+  // Handle form Data Changes
+  const handleChange = (e => {  
+    setFormData({
+      ...formData,
+      [e.target.value]: e.target.value,
+    })
+  });   
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -17,11 +34,41 @@ function Login() {
       // Redirect to dashboard
       window.location.href = "/";
 
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
+      setError(err.response?.data?.message || "Login Failed")
     }
   };
 
+   return (
+    <div className="auth-page">
+      <form className="auth-form" onSubmit={handleSubmit}>
+        <h1>Login</h1>
+
+        {error && <p className="error-message">{error}</p>}
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
