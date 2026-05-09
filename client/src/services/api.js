cc3969b84c4aff57274c021d49e6c1563fb3756f
Ray-Xavier-2021 <horatioxhanley@gmail.com>
2026-05-08 21:43:06 -0400

    Update services dir

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,16 +1,26 @@
 import axios from "axios";
 
+// Get user Token
+const token = localStorage.getItem("token");
+
+// Configure
+const config = {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  }
+}
+
 // Backend API URL
 const API_URL = "http://localhost:5000/api/entries";
 
 // Get Entries
-export const getEntries = () => axios.get(API_URL)
+export const getEntries = () => axios.get(API_URL, config)
 
 // Create Entry
-export const createEntry = (entryData) => axios.post(API_URL, entryData)
+export const createEntry = (entryData) => axios.post(API_URL, entryData, config)
 
 // Update Entry
-export const updateEntry = (id, entryData) => axios.put(`${API_URL}/${id}`, entryData)
+export const updateEntry = (id, entryData) => axios.put(`${API_URL}/${id}`, entryData, config)
 
 // Delete Entry
-export const deleteEntry = (id) => axios.delete(`${API_URL}/${id}`)
+export const deleteEntry = (id) => axios.delete(`${API_URL}/${id}`, config)
--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+// Backend URL
 const API_URL = "http://localhost:5000/api/auth";
 
+// Register User
 export const registerUser = (userData) =>
   axios.post(`${API_URL}/register`, userData);
 
+// Login User
 export const loginUser = (userData) =>
   axios.post(`${API_URL}/login`, userData);
