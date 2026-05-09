dbfcd038ae87059468a07834b23d40c59b4be30f
Ray-Xavier-2021 <horatioxhanley@gmail.com>
2026-05-09 13:09:58 -0400

    Update api.js file

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,26 +1,28 @@
 import axios from "axios";
 
-// Get user Token
-const token = localStorage.getItem("token");
-
-// Configure
-const config = {
-  headers: {
-    Authorization: `Bearer ${token}`,
-  }
-}
-
-// Backend API URL
 const API_URL = "http://localhost:5000/api/entries";
 
+const getConfig = () => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+};
+
 // Get Entries
-export const getEntries = () => axios.get(API_URL, config)
+export const getEntries = () => axios.get(API_URL, getConfig());
 
 // Create Entry
-export const createEntry = (entryData) => axios.post(API_URL, entryData, config)
+export const createEntry = (entryData) =>
+  axios.post(API_URL, entryData, getConfig());
 
 // Update Entry
-export const updateEntry = (id, entryData) => axios.put(`${API_URL}/${id}`, entryData, config)
+export const updateEntry = (id, entryData) =>
+  axios.put(`${API_URL}/${id}`, entryData, getConfig());
 
 // Delete Entry
-export const deleteEntry = (id) => axios.delete(`${API_URL}/${id}`, config)
+export const deleteEntry = (id) =>
+  axios.delete(`${API_URL}/${id}`, getConfig());
